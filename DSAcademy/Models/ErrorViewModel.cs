851bb9667850a6f08d41eb24f5cb248f6cadd0da
DevEriwa <132900490+DevEriwa@users.noreply.github.com>
2024-07-21 13:34:20 +0100

    Add project files.

---
+++ DSAcademy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DSAcademy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
