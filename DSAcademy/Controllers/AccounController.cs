8a9db0a086a3fb95e365087a86d4a936198ab00d
DevEriwa <132900490+DevEriwa@users.noreply.github.com>
2024-07-21 15:07:49 +0100

    work on the set up in account controller

---
+++ DSAcademy/Controllers/AccounController.cs
@@ -0,0 +1,33 @@
+﻿using Core.Db;
+using Core.Models;
+using Logic.IHelpers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DSAcademy.Controllers
+{
+	public class AccountsController : Controller
+	{
+		private readonly AppDbContext _context;
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly SignInManager<ApplicationUser> _signInManager;
+		private readonly IUserHelper _userHelper; 
+
+		public AccountsController
+			(AppDbContext context,
+			UserManager<ApplicationUser> userManger,
+			SignInManager<ApplicationUser> signInManager,
+			IUserHelper userHelper)
+		{
+			_context = context;
+			_userManager = userManger;
+			_signInManager = signInManager;
+			_userHelper = userHelper;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
