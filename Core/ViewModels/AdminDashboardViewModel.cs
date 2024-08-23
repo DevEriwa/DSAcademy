c5247cc749771f82cd9fbef56cc3ae0aacebaac8
Israel Ezugwu <ezugwuisrael6@gmail.com>
2024-08-23 15:48:20 +0100

    I worked on the admin inde and controller

---
+++ Core/ViewModels/AdminDashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.ViewModels
+{
+    public class AdminDashboardViewModel
+    {
+        public int StudentCount { get; set; }
+        public int TeacherCount { get; set; }
+        public int CourseCount { get; set; }
+        public List<ApplicationUserViewModel>? Student { get; set; }
+        public List<ApplicationUserViewModel>? Teacher { get; set; }
+        public List<TrainingCourseViewModel>? Courses { get; set; }
+        public Guid? CompanyId { get; set; }
+        public string? FullName { get; set; }
+    }
+}
